d8982ec4afc7bfd610e80750ae95663cff0e5c45
aj-learnings <aayushjalan.learning@gmail.com>
2024-09-17 21:30:22 +0530

    returning same error response in custom auth entry point.

--- src/main/java/com/ajlearnings/workbuddy/filters/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/ajlearnings/workbuddy/filters/CustomAuthenticationEntryPoint.java
@@ -1,12 +1,17 @@
 package com.ajlearnings.workbuddy.filters;
 
+import com.ajlearnings.workbuddy.model.response.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
@@ -15,7 +20,17 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json");
-        response.getWriter().write("{\"message\": \"You are not authenticated.\"}");
+        var guid = UUID.randomUUID().toString();
+        var errorResponse = ErrorResponse.builder()
+                                        .guid(guid)
+                                        .message("You are not authenticated")
+                                        .statusCode(HttpStatus.UNAUTHORIZED.value())
+                                        .statusName(HttpStatus.UNAUTHORIZED.name())
+                                        .path(request.getRequestURI())
+                                        .method(request.getMethod())
+                                        .build();
+        ObjectMapper mapper = new ObjectMapper();
+        response.getWriter().write(mapper.writeValueAsString(errorResponse));
     }
 }
 

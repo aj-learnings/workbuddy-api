ad579ee01ded832b2522074984705fcb6a5fdd48
aj-learnings <aayushjalan.learning@gmail.com>
2024-09-22 12:35:51 +0530

    different cache ttl's handling.

--- src/main/java/com/ajlearnings/workbuddy/config/RedisConfig.java
+++ src/main/java/com/ajlearnings/workbuddy/config/RedisConfig.java
@@ -1,15 +1,21 @@
 package com.ajlearnings.workbuddy.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.HashMap;
 
 @Configuration
 @EnableCaching
@@ -23,6 +29,12 @@ public class RedisConfig {
 
     @Value("${spring.data.redis.password}")
     private String redisPassword;
+
+    @Value("${spring.cache.time-to-live.general}")
+    private int generalTTL;
+
+    @Value("${spring.cache.time-to-live.otp}")
+    private int otpTTL;
 
     @Bean public RedisConnectionFactory redisConnectionFactory() {
         var redisStandaloneConfiguration = new RedisStandaloneConfiguration();
@@ -39,5 +51,20 @@ public class RedisConfig {
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+
+        var defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                                                        .entryTtl(Duration.ofSeconds(generalTTL));
+
+        var cacheConfigurations = new HashMap<String, RedisCacheConfiguration>();
+        cacheConfigurations.put("otp", defaultCacheConfig.entryTtl(Duration.ofSeconds(otpTTL)));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                                .cacheDefaults(defaultCacheConfig)
+                                .withInitialCacheConfigurations(cacheConfigurations)
+                                .build();
     }
 }

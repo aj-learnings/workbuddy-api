08e49896a5060215c9254ef3033e78d0986e47f3
aj-learnings <aayushjalan.learning@gmail.com>
2024-09-26 19:14:45 +0530

    otp generating method changes

--- src/main/java/com/ajlearnings/workbuddy/service/OtpService.java
+++ src/main/java/com/ajlearnings/workbuddy/service/OtpService.java
@@ -1,23 +1,29 @@
 package com.ajlearnings.workbuddy.service;
 
+import com.ajlearnings.workbuddy.Constants;
 import com.ajlearnings.workbuddy.exception.ResourceNotFoundException;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
 import java.util.Random;
 
 @Service
 @CacheConfig(cacheNames = "otp")
 public class OtpService implements IOtpService {
 
-    private static final Random random = new Random();
+    private static final Random random = new SecureRandom();
 
     @Override
     @CachePut(key = "#uniqueIdentifier")
     public long generateOTP(String uniqueIdentifier, int digits) {
-        return (long) Math.pow(10, digits-1) + random.nextLong((long) (9 * Math.pow(10, digits-1)));
+        var builder = new StringBuilder();
+        for (int i=0; i<digits; i++) {
+            builder.append(Constants.NUMBERS.charAt(random.nextInt(Constants.NUMBERS.length())));
+        }
+        return Long.parseLong(builder.toString());
     }
 
     @Override
